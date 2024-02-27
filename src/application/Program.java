7427228cb20fde5dade2d8a080b2e36ea91e609a
Iryugiri <gabriel.gomes.c2006@gmail.com>
2024-02-27 15:55:00 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,11 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		System.out.println("================ \n"+ obj);
+		
+		Seller slr = new Seller(1, "Pedro", "pedro@gmail.com", new Date() , 1000.0 , obj);
+		
+		System.out.println("================ \n"+ slr);
 	}
 
 }
